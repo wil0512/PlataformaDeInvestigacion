335df3bb3fc3c3538c1eba4f4c2d64037cd83c62
wilson <alcocerqwilson@hotmail.com>
2017-10-06 00:48:07 -0400

    LogIn Wilson
    La carpeta librerias es para la conectividad con la bd el back up es para colocar el modelo de la bd

---
+++ Login Wilson/SIS2/src/org/umss/model/Usuario.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.umss.model;
+
+/**
+ *
+ * @author wilson
+ */
+public class Usuario {
+    private String nombre;
+    private String password;
+
+    public Usuario(String nombre, String password) {
+        this.nombre = nombre;
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
